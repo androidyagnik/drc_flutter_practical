0f582090203ea0f5d60adec3e79c8c4b7990d39f
yagnik <yagnikkateliya@gmail.com>
2022-05-14 13:26:27 +0530

    - Home screen-> See More Categories

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -2,3 +2,4 @@ const String app_name = "DRC Practical";
 const String lblBestSeller = "Best Seller";
 const String lblSeeAll = "See all";
 const String lblAvailable = "Available";
+const String lblSeeMoreCategories = "See More Categories";
--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -17,8 +17,10 @@ class HomeView extends GetView<HomeController> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            widgetBannerSlider(),
             widgetBestSeller().marginOnly(top: size_150),
-            getOfferItemBanner()
+            widgetOfferItemBanner(),
+            widgetSeeMoreCategories()
           ],
         ),
       ),
@@ -44,10 +46,10 @@ class HomeView extends GetView<HomeController> {
             children: [
               Expanded(
                   child: Text(
-                    lblBestSeller,
-                    style: AppText.textBold
-                        .copyWith(color: colorBlack, fontSize: font_18),
-                  )),
+                lblBestSeller,
+                style: AppText.textBold
+                    .copyWith(color: colorBlack, fontSize: font_18),
+              )),
               Text(lblSeeAll,
                   style: AppText.textRegular
                       .copyWith(color: colorPink, fontSize: font_16))
@@ -115,25 +117,19 @@ class HomeView extends GetView<HomeController> {
     return itemWidget;
   }
 
-  Widget getOfferItemBanner() {
+  Widget widgetOfferItemBanner() {
     List<Widget> itemWidget = [];
 
     for (int i = 0; i < 10; i++) {
-      itemWidget.add(
-          ClipRRect(
-            child: Container(
-              width: (MediaQuery
-                  .of(Get.context!)
-                  .size
-                  .width - size_20),
-              height: size_150,
-              decoration: BoxDecoration(
-                  color: colorLightPink,
-                  borderRadius: BorderRadius.circular(size_10)
-              ),
-            ),
-          ).marginOnly(right: i ==9 ? size_0:size_20)
-      );
+      itemWidget.add(ClipRRect(
+        child: Container(
+          width: (MediaQuery.of(Get.context!).size.width - size_20),
+          height: size_150,
+          decoration: BoxDecoration(
+              color: colorLightPink,
+              borderRadius: BorderRadius.circular(size_10)),
+        ),
+      ).marginOnly(right: i == 9 ? size_0 : size_20));
     }
 
     return SingleChildScrollView(
@@ -145,7 +141,83 @@ class HomeView extends GetView<HomeController> {
     );
   }
 
-  Widget widgetSeeMoreCategories(){
-    return Container();
+  Widget widgetSeeMoreCategories() {
+    return Container(
+      margin: EdgeInsets.only(top: size_10),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            lblSeeMoreCategories,
+            textAlign: TextAlign.left,
+            style:
+                AppText.textBold.copyWith(color: colorBlack, fontSize: font_18),
+          ).paddingOnly(left: size_20),
+          itemWidgetSeeMoreCategories()
+        ],
+      ),
+    );
+  }
+
+  Widget itemWidgetSeeMoreCategories() {
+    List<Widget> itemWidget = [];
+    for (int i = 0; i < 10; i++) {
+      itemWidget.add(Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: size_85,
+              height: size_85,
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      width: size_80,
+                      height: size_80,
+                      decoration: BoxDecoration(
+                          color: colorLightGrey,
+                          borderRadius: BorderRadius.circular(size_40),
+                          border: Border.all(color: colorPink, width: size_1)),
+                    ),
+                  ),
+                  Container(
+                    width: size_80,
+                    height: size_80,
+                    decoration: BoxDecoration(
+                      color: colorBlack,
+                      borderRadius: BorderRadius.circular(size_40),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: size_90,
+              child: Text(
+                "Category Name",
+                maxLines: 2,
+                softWrap: true,
+                textAlign: TextAlign.center,
+                style: AppText.textRegular
+                    .copyWith(color: colorBlack, fontSize: font_16),
+              ).marginOnly(top: size_10),
+            )
+          ],
+        ).marginOnly(right: i == 9 ? size_0 : size_20),
+      ));
+    }
+
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      padding: EdgeInsets.only(top: size_20, left: size_20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: itemWidget,
+      ),
+    );
   }
 }
