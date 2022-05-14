246e1bccba7b2b42775522ef1030ae2eb7eb0833
yagnik <yagnikkateliya@gmail.com>
2022-05-14 13:47:54 +0530

    - Home screen-> Shop By Brand

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -3,3 +3,4 @@ const String lblBestSeller = "Best Seller";
 const String lblSeeAll = "See all";
 const String lblAvailable = "Available";
 const String lblSeeMoreCategories = "See More Categories";
+const String lblShopByBrand = "Shop By Brand";
--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -14,13 +14,16 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      backgroundColor: colorWhite,
       body: SingleChildScrollView(
         child: Column(
           children: [
             widgetBannerSlider(),
             widgetBestSeller().marginOnly(top: size_150),
             widgetOfferItemBanner(),
-            widgetSeeMoreCategories()
+            widgetSeeMoreCategories(),
+            widgetProductWithCategory(),
+            widgetShopByBrands()
           ],
         ),
       ),
@@ -32,54 +35,7 @@ class HomeView extends GetView<HomeController> {
   }
 
   Widget widgetBestSeller() {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          width: double.infinity,
-          padding: EdgeInsets.all(size_15),
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(bgBestSeller), fit: BoxFit.fill)),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                  child: Text(
-                lblBestSeller,
-                style: AppText.textBold
-                    .copyWith(color: colorBlack, fontSize: font_18),
-              )),
-              Text(lblSeeAll,
-                  style: AppText.textRegular
-                      .copyWith(color: colorPink, fontSize: font_16))
-            ],
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(size_15),
-          decoration: BoxDecoration(color: colorLightGrey),
-          child: Container(
-            decoration: BoxDecoration(
-              color: colorWhite,
-            ),
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.only(
-                  left: size_10, top: size_10, bottom: size_10),
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: itemWidgetBestSeller(),
-              ),
-            ),
-          ),
-        )
-      ],
-    );
-  }
-
-  List<Widget> itemWidgetBestSeller() {
     List<Widget> itemWidget = [];
-
     for (int i = 0; i < 20; i++) {
       itemWidget.add(SizedBox(
         width: size_150,
@@ -89,7 +45,7 @@ class HomeView extends GetView<HomeController> {
           children: [
             Container(
               height: size_250,
-              decoration: BoxDecoration(color: colorLightGrey),
+              decoration: const BoxDecoration(color: colorLightGrey),
             ),
             Text(
               lblAvailable.toUpperCase(),
@@ -114,7 +70,47 @@ class HomeView extends GetView<HomeController> {
       ).marginOnly(right: size_10));
     }
 
-    return itemWidget;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(size_15),
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(bgBestSeller), fit: BoxFit.fill)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: Text(
+                lblBestSeller,
+                style: AppText.textBold
+                    .copyWith(color: colorBlack, fontSize: font_18),
+              )),
+              Text(lblSeeAll,
+                  style: AppText.textRegular
+                      .copyWith(color: colorPink, fontSize: font_16))
+            ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(size_15),
+          decoration: const BoxDecoration(color: colorLightGrey),
+          child: Container(
+            decoration: const BoxDecoration(
+              color: colorWhite,
+            ),
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(
+                  left: size_10, top: size_10, bottom: size_10),
+              scrollDirection: Axis.horizontal,
+              child: Row(children: itemWidget),
+            ),
+          ),
+        )
+      ],
+    );
   }
 
   Widget widgetOfferItemBanner() {
@@ -123,7 +119,7 @@ class HomeView extends GetView<HomeController> {
     for (int i = 0; i < 10; i++) {
       itemWidget.add(ClipRRect(
         child: Container(
-          width: (MediaQuery.of(Get.context!).size.width - size_20),
+          width: (MediaQuery.of(Get.context!).size.width - size_40),
           height: size_150,
           decoration: BoxDecoration(
               color: colorLightPink,
@@ -134,7 +130,7 @@ class HomeView extends GetView<HomeController> {
 
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      padding: EdgeInsets.all(size_20),
+      padding: const EdgeInsets.all(size_20),
       child: Row(
         children: itemWidget,
       ),
@@ -143,7 +139,7 @@ class HomeView extends GetView<HomeController> {
 
   Widget widgetSeeMoreCategories() {
     return Container(
-      margin: EdgeInsets.only(top: size_10),
+      margin: const EdgeInsets.only(top: size_10, bottom: size_20),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -213,11 +209,135 @@ class HomeView extends GetView<HomeController> {
 
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      padding: EdgeInsets.only(top: size_20, left: size_20),
+      padding: const EdgeInsets.only(top: size_20, left: size_20),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: itemWidget,
       ),
     );
+  }
+
+  Widget widgetProductWithCategory() {
+    List<Widget> itemWidget = [];
+    for (int i = 0; i < 20; i++) {
+      itemWidget.add(SizedBox(
+        width: size_150,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: size_250,
+              decoration: const BoxDecoration(color: colorLightGrey),
+            ),
+            Text(
+              lblAvailable.toUpperCase(),
+              style: AppText.textRegular
+                  .copyWith(color: Colors.grey, fontSize: font_16),
+            ).marginOnly(top: size_5),
+            Text(
+              "Item Name",
+              style: AppText.textBold
+                  .copyWith(color: colorBlack, fontSize: font_18),
+              softWrap: true,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ).marginOnly(top: size_3),
+            Text(
+              "AED 12.0",
+              style: AppText.textBold
+                  .copyWith(color: colorPink, fontSize: font_16),
+            ).marginOnly(top: size_12),
+          ],
+        ),
+      ).marginOnly(right: size_10));
+    }
+
+    return Container(
+      decoration: BoxDecoration(color: colorLightGrey),
+      padding: EdgeInsets.all(size_20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: Text(
+                lblBestSeller,
+                style: AppText.textBold
+                    .copyWith(color: colorBlack, fontSize: font_18),
+              )),
+              Text(lblSeeAll,
+                  style: AppText.textRegular
+                      .copyWith(color: colorPink, fontSize: font_16))
+            ],
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              color: colorWhite,
+            ),
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(
+                  left: size_10, top: size_10, bottom: size_10),
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: itemWidget,
+              ),
+            ),
+          ).marginOnly(top: size_20),
+        ],
+      ),
+    );
+  }
+
+  Widget widgetShopByBrands() {
+    List<Widget> itemWidget = [];
+    for (int i = 0; i < 10; i++) {
+      itemWidget.add(
+        Container(
+          width: size_100,
+          height: size_100,
+          child: Stack(
+            children: [
+              Image.asset(bgBrandHome,width: size_100,height: size_100,fit: BoxFit.fill,),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: size_60,
+                  height: size_60,
+                  decoration: BoxDecoration(
+                    color: colorBlack,
+                    borderRadius: BorderRadius.circular(size_40),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ).marginOnly(right: i == 9 ? size_0 : size_20),
+      );
+    }
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          lblShopByBrand,
+          textAlign: TextAlign.left,
+          style:
+              AppText.textBold.copyWith(color: colorBlack, fontSize: font_18),
+        ).paddingOnly(left: size_20),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.only(top: size_20, left: size_20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: itemWidget,
+          ),
+        )
+      ],
+    ).marginSymmetric(vertical: size_20);
   }
 }
